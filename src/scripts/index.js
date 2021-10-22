a31a466ebd9c8d90e1a02e68708e2d8f8e50d6ac
Mikhail Kucherenko <admin@kimc.ms>
2021-10-22 12:26:13 +0700

    Paging bug fixed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,7 +10,7 @@ const popupContainer = document.querySelector('.popup .content');
 const popupClose = document.querySelector('.popup .action');
 const loader = document.querySelector('.loader');
 
-const MAX_PAGE_IAMGES = 34;
+const MAX_PAGE_IMAGES = 3;
 let loaderTimeout;
 
 /**
@@ -152,14 +152,14 @@ const togglePopup = function () {
  */
 const actionHandler = function (evt) {
     evt.preventDefault();
-    const nextPage = evt.currentTarget.dataset.page;
-    evt.currentTarget.dataset.page = nextPage + 1;
+    const nextPage = parseInt(evt.currentTarget.dataset.page, 10) + 1;
+	getPictures(nextPage);
 
-    if (nextPage > MAX_PAGE_IAMGES) {
-        console.warn(`WARN: You are trying to call a page that exceeds ${MAX_PAGE_IAMGES}`);
+    if (nextPage >= MAX_PAGE_IMAGES) {
+        console.warn(`WARN: You have reached last page (${MAX_PAGE_IMAGES})`);
         evt.currentTarget.disabled = true;
     } else {
-        getPictures(nextPage);
+	    evt.currentTarget.dataset.page = nextPage;
     }
 }
 
